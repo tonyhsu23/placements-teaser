df39a303b993b54c7cb8cc399955d6eeab210711
Tony Hsu <t153426789@gmail.com>
2020-11-17 16:56:48 +0800

    Setting up the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'invoice/index'
-  get 'invoice/example'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'invoice#index'
+  resources :line_items, only: :index
+
+  root 'line_items#index'
 end
