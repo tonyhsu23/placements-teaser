fca54dbc0c09f0ffc8d0fe29e3f400d90f24623f
Tony Hsu <t153426789@gmail.com>
2020-11-18 10:38:35 +0800

    Refactor navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def nav_item
+    content_tag(:ul, class: 'navbar-nav') do
+      yield
+    end
+  end
+
+  def nav_link(text, path)
+    active = text.parameterize.underscore == controller_name ? 'active' : ''
+
+    content_tag(:li, class: "nav-item #{active}") do
+      link_to(text, path, class: 'nav-link')
+    end
+  end
 end
