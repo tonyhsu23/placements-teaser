75b2350283df4ecccc949a5090846aa494607101
Tony Hsu <t153426789@gmail.com>
2020-11-18 10:39:05 +0800

    Create campaign index page

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,9 @@
 class CampaignsController < ApplicationController
+  def index
+    @campaigns = Campaign.includes(:line_items)
+    @campaigns = Pagination.new(@campaigns, paginate_params).paginate
+  end
+
   def show
     @campaign = Campaign.includes(:line_items).find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :campaigns, only: :show
+  resources :campaigns, only: %i[index show]
   resources :line_items, only: %i[index update]
 
   root 'line_items#index'
